a6613c0b132e6510a638464691b4458c0e83f901
ZHY45022 <397945022@qq.com>
2019-06-19 15:38:48 +0800

    张宏野

---
+++ src/com/hqyj/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.hqyj.dao;
+
+public interface UserDao {
+
+}
---
+++ src/com/hqyj/dao/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.hqyj.dao.impl;
+
+import com.hqyj.dao.UserDao;
+
+public class UserDaoImpl implements UserDao{
+
+}
